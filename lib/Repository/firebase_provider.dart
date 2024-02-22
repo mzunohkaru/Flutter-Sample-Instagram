3e8bb093b519741fcb80354b7ab50f63fa2353cd
水野光琉 <99012157+mzunohkaru@users.noreply.github.com>
2024-02-23 00:17:23 +0900

    Delete lib directory

--- lib/Repository/firebase_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:instagram_clone/Utils/constant.dart';
-
-// 現在のユーザーIDを提供するプロバイダ
-final currentUserUidProvider = StateProvider<String?>((ref) {
-  return auth.currentUser?.uid;
-});
-
-
