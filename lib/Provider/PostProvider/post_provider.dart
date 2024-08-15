1e124b2d5894d18911cdc3685b37e52a45ee938a
mzunohkaru <winkaisyu@gmail.com>
2024-08-15 12:33:51 +0900

    comment out:post provider

--- lib/Provider/PostProvider/post_provider.dart
+++ lib/Provider/PostProvider/post_provider.dart
@@ -1,54 +1,54 @@
-import 'dart:async';
-import 'package:instagram_clone/Model/Entity/Post/post.dart';
-import 'package:instagram_clone/Utils/constant.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-part 'post_provider.g.dart';
+// import 'dart:async';
+// import 'package:instagram_clone/Model/Entity/Post/post.dart';
+// import 'package:instagram_clone/Utils/constant.dart';
+// import 'package:riverpod_annotation/riverpod_annotation.dart';
+// part 'post_provider.g.dart';
 
-@riverpod
-class PostProvider extends _$PostProvider {
-  // 投稿データを取得・監視
-  @override
-  Stream<List<Post>> build() async* {
-    logger.d("Call: PostProvider");
+// @riverpod
+// class PostProvider extends _$PostProvider {
+//   // 投稿データを取得・監視
+//   @override
+//   Stream<List<Post>> build() async* {
+//     logger.d("Call: PostProvider");
 
-    try {
-      final currentUserUid = auth.currentUser?.uid;
-      if (currentUserUid == null) {
-        throw Exception('DEBUG: Not found user ID');
-      }
+//     try {
+//       final currentUserUid = auth.currentUser?.uid;
+//       if (currentUserUid == null) {
+//         throw Exception('DEBUG: Not found user ID');
+//       }
 
-      final postSnapshotStream = PostCollections.snapshots();
+//       final postSnapshotStream = PostCollections.snapshots();
 
-      await for (final postSnapshot in postSnapshotStream) {
-        final posts = await Future.wait(postSnapshot.docs.map((doc) async {
-          try {
-            final postData = doc.data();
-            final userDoc =
-                await UserCollections.doc(postData['ownerUid']).get();
-            final userData = userDoc.data();
-            if (userData == null) {
-              return null;
-            }
-            // Post型に変換
-            final post = Post.fromJson({...postData, 'user': userData});
-            final didLike =
-                postData['likeUsers']?.contains(currentUserUid) ?? false;
-            return post.copyWith(didLike: didLike);
-          } catch (e) {
-            logger.e("DEBUG: Failed to fetching post data with ${doc.id}", error: e);
-            rethrow;
-          }
-        }).toList());
+//       await for (final postSnapshot in postSnapshotStream) {
+//         final posts = await Future.wait(postSnapshot.docs.map((doc) async {
+//           try {
+//             final postData = doc.data();
+//             final userDoc =
+//                 await UserCollections.doc(postData['ownerUid']).get();
+//             final userData = userDoc.data();
+//             if (userData == null) {
+//               return null;
+//             }
+//             // Post型に変換
+//             final post = Post.fromJson({...postData, 'user': userData});
+//             final didLike =
+//                 postData['likeUsers']?.contains(currentUserUid) ?? false;
+//             return post.copyWith(didLike: didLike);
+//           } catch (e) {
+//             logger.e("DEBUG: Failed to fetching post data with ${doc.id}", error: e);
+//             rethrow;
+//           }
+//         }).toList());
 
-        // nullを除外し、Post型にキャストした後、リストとしてストリームに渡す
-        yield posts.where((post) => post != null).cast<Post>().toList();
-      }
-    } catch (e) {
-      logger.e('DEBUG: Failed to fetch all post data', error: e);
-      rethrow;
-    }
-  }
-}
+//         // nullを除外し、Post型にキャストした後、リストとしてストリームに渡す
+//         yield posts.where((post) => post != null).cast<Post>().toList();
+//       }
+//     } catch (e) {
+//       logger.e('DEBUG: Failed to fetch all post data', error: e);
+//       rethrow;
+//     }
+//   }
+// }
 
 /*
 // StreamProvider
